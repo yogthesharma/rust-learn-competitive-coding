5598430b84b7079298b03da6f2f25b34432a7ad2
Yog Sharma <yogthesharma@gmail.com>
2024-10-15 11:59:08 +0530

    Fixed the folder structure for rust running

--- rust/staircase/mod.rs
+++ rust/staircase/mod.rs
@@ -1,4 +1,4 @@
-fn staircase() {
+pub fn staircase(size: i32) {
     for i in 1..=size {
         let spaces = " ".repeat((size - i) as usize);
         let hashes = "#".repeat(i as usize);
--- staircase/staircase.js
+++
@@ -1,7 +0,0 @@
-function staircase(n) {
-  for (let i = 1; i <= size; i++) {
-    console.log(" ".repeat(size - i) + "#".repeat(i));
-  }
-}
-
-staircase(5);
